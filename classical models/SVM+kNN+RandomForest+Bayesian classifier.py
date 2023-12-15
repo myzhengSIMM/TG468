506ca14c8349f13a6414eb875d0ad8681bf9a1e0
kunwang927 <153788288+kunwang927@users.noreply.github.com>
2023-12-15 22:40:46 +0800

    Update SVM+kNN+RandomForest+Bayesian classifier.py

--- classical models/SVM+kNN+RandomForest+Bayesian classifier.py
+++ classical models/SVM+kNN+RandomForest+Bayesian classifier.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 import numpy as np
 import torch
@@ -10,7 +9,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 seed = 10
-
 os.environ['PYTHONHASHSEED'] = str(seed)    
 torch.manual_seed(seed)
 torch.cuda.manual_seed(seed)
@@ -21,31 +19,23 @@ torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False  
 torch.backends.cudnn.enabled = True 
 
-
-
 test_set_name = 'Hellmann'   #  or Liu  
 time = 'PFS_MONTHS'          #  OS_MONTHS
 time_status = 'PFS_STATUS'   #  OS_STATUS
 
- 
 train_set = pd.read_csv('../data/train_set.csv')
 train_set = shuffle(train_set)
 test_set = pd.read_csv('../data/test_set.csv')
 
-
 MUTsentence = pd.concat([train_set,test_set],axis=0)
 
-
 lines = list(MUTsentence['mutsentence'])
-
 label = list(MUTsentence['label'])
 label = np.array(label)
-
 Lines = [line.split() for line in lines]
 selected_gene  = pd.read_csv('../data/candidate gene set.csv')
 selected_gene = selected_gene[selected_gene['p_value']<=0.05]
 selected_gene = list(selected_gene['gene_name'])
-
 Sentence = []
 for line in Lines:
     new_sentence = []
@@ -58,23 +48,13 @@ for line in Lines:
 Sentence = np.array(Sentence)
 Sentence.shape
 
-
-
-
 TRAIN_SIZE = train_set.shape[0]
-
 X_train, y_train = Sentence[:TRAIN_SIZE], label[:TRAIN_SIZE]
-
 X_test, y_test = Sentence[TRAIN_SIZE:],label[TRAIN_SIZE:]
-
-
 X_train = pd.DataFrame(data=X_train, columns=selected_gene)
 y_train = pd.DataFrame(data=y_train, columns=['target'])
-
 X_test = pd.DataFrame(data=X_test, columns=selected_gene)
 y_test = pd.DataFrame(data=y_test, columns=['target'])
-
-
 "SVM"
 from sklearn.svm import SVC
 model = SVC()
@@ -99,8 +79,6 @@ y_pred = model.predict(X_test)
 # model.fit(X_train, y_train)
 # y_pred = model.predict(X_test)
 
-
-
 time_info = test_set
 
 test_PFS = list(time_info[time])
@@ -116,12 +94,9 @@ test_pred['ID'] = ID
 test_pred.replace('0:Censor','0',inplace = True)
 test_pred.replace('1:Event','1',inplace = True)
 
-
 test_pred['response_pred'] = y_pred  
 
-
 test_pred['Gender'] = list(test_set['Gender']) 
-
 if test_set_name == 'Hellmann':
     AGE = []
     for a in list(test_set['Age']):
@@ -130,7 +105,7 @@ if test_set_name == 'Hellmann':
         elif a<65:
             AGE.append(0)
     test_pred['AGE'] = AGE
-    
+
 TMB = []
 for a in list(test_set['nonsynonymous_somatic_mutations_counts']):
     if a>=380:
@@ -151,8 +126,3 @@ print('sensitivuty,specificity = ',sensitivuty,specificity)
 print('Accuracy = ', (TP + TN)/(TN+FP+FN+TP))      
 print('ppv,npv = ',ppv,npv)    
 print('portion = ' , y_pred.sum(), round(y_pred.sum()/y_pred.shape[0],4))
-
-
-
-
-
