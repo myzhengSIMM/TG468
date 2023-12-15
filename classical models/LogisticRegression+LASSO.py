2d8dd851af4a0be553131e35a9401787e86663f8
kunwang927 <153788288+kunwang927@users.noreply.github.com>
2023-12-15 22:38:14 +0800

    Update LogisticRegression+LASSO.py

--- classical models/LogisticRegression+LASSO.py
+++ classical models/LogisticRegression+LASSO.py
@@ -10,7 +10,6 @@ import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model import Lasso
 
-
 train_set = pd.read_csv('../data/train_set.csv')
 train_set = shuffle(train_set)
 test_set = pd.read_csv('../data/test_set.csv')
@@ -35,24 +34,17 @@ for line in Lines:
             new_sentence.append(0)
     Sentence.append(new_sentence)
 Sentence = np.array(Sentence)
-
-
 TRAIN_SIZE = train_set.shape[0]
 X_train, y_train = Sentence[:TRAIN_SIZE], label[:TRAIN_SIZE]
 X_test, y_test = Sentence[TRAIN_SIZE:],label[TRAIN_SIZE:]
-
 
 "LogisticRegression"
 moedel = LogisticRegression()
 "LASSO"
 #moedel = Lasso(alpha = 0.0)
-
-
 moedel.fit(X_train, y_train)
-
 y_pred_test = moedel.predict(X_test)
 y_pred_train = moedel.predict(X_train)
-
 
 def get_roc_coordinatesANDpred(labels_true,prob_pred,data_catalog):
     fpr, tpr, threshold = roc_curve(labels_true,prob_pred)
@@ -65,8 +57,5 @@ def get_roc_coordinatesANDpred(labels_true,prob_pred,data_catalog):
     save_pred['labels'] =  [labels_true[j][0] for j in range(len(labels_true))]
     save_pred['prob'] = [prob_pred[j][0] for j in range(len(prob_pred))]
     save_pred.to_csv('../results/ICI-roc_' + data_catalog + '.csv',index = False)
-    
 get_roc_coordinatesANDpred(y_train, y_pred_train,'train')  
 get_roc_coordinatesANDpred(y_test, y_pred_test,'test')   
-
-
