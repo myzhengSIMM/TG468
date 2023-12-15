6935198721a807af65921d050abb3d0dd15164db
kunwang927 <153788288+kunwang927@users.noreply.github.com>
2023-12-15 22:43:44 +0800

    Update gcn.py

--- models/gcn.py
+++ models/gcn.py
@@ -5,8 +5,6 @@ from torch import Tensor
 import math
 import torch.nn.functional as F
 
-
-
 class GraphConvolution(nn.Module):
     def __init__( self, input_dim, \
                         output_dim, \
@@ -18,24 +16,18 @@ class GraphConvolution(nn.Module):
         super(GraphConvolution, self).__init__()
         self.support = support
         self.featureless = featureless
-
         for i in range(len(self.support)):
             setattr(self, 'W{}'.format(i), nn.Parameter(torch.randn(input_dim, output_dim))) 
-        
         self.bias = bias
         if self.bias:
             self.b = nn.Parameter(torch.zeros(1, output_dim))
-
         self.act_func = act_func
         self.dropout = nn.Dropout(dropout_rate)
-        
         self.reset_parameters()  
-    
     def reset_parameters(self):   
         nn.init.xavier_uniform_(getattr(self, 'W{}'.format(0)), gain=1)     
         if self.bias:
             nn.init.zeros_(self.b) 
-        
     def forward(self, x):
         x = self.dropout(x)
         for i in range(len(self.support)):
@@ -55,7 +47,6 @@ class GraphConvolution(nn.Module):
         self.embedding = out        
         return out
 
-
 class GCN(nn.Module):
     def __init__( self, input_dim, \
                         support,\
@@ -64,12 +55,7 @@ class GCN(nn.Module):
         super(GCN, self).__init__()
         self.layer1 = GraphConvolution(input_dim, 200, support, act_func=nn.ReLU(), featureless=True, dropout_rate=dropout_rate)
         self.layer2= GraphConvolution(200, 1, support, act_func=nn.Sigmoid(),dropout_rate=dropout_rate)    
-        
     def forward(self, x):
         out = self.layer1(x)
         out = self.layer2(out)
         return out
-    
-    
-    
-    
